f718bb45fe5712032f11465298cde34887b86475
ETHANCPIPKIN <lucidfridaynights@gmail.com>
2021-06-14 23:42:57 -0700

    cleaned up the code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// Initialize the Image Classifier method with MobileNet. A callback needs to be passed.
+
 let classifier;
 
-// A variable to hold the image we want to classify
 let img;
 
 function preload() {
@@ -15,13 +14,11 @@ function setup() {
   image(img, 0, 0);
 }
 
-// A function to run when we get any errors and the results
 function gotResult(error, results) {
   // Display error in the console
   if (error) {
     console.error(error);
   } else {
-    // The results are in an array ordered by confidence.
     console.log(results);
     createDiv(`Label: ${results[0].label}`);
     createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`);
